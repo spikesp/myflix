e4bdfc2d49f22733827700a8d87127193b38c347
spike <weisuojiaoshou@gmail.com>
2018-01-23 12:08:34 +0900

    Finish the user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
 
   has_many :queue_items, -> { order("position") }
+  has_many :reviews, -> { order("created_at DESC")}
   has_secure_password validations: false
 
   
