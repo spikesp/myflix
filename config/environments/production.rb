787af6d7d762de33192e987d2af4d8020f411d57
spike <weisuojiaoshou@gmail.com>
2018-01-29 16:13:00 +0900

    try to configure mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,16 @@ Myflix::Application.configure do
 
   config.assets.digest = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandbox076523e29eda45448fef821b52d7b95d.mailgun.org",
+    :user_name => "postmaster@sandbox076523e29eda45448fef821b52d7b95d.mailgun.org",
+    :password => "myflex"
+  }
+
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
