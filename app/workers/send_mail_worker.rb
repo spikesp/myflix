496b849e17f609fdf3fa4b0b71d1990edd04ce34
spike <weisuojiaoshou@gmail.com>
2018-01-30 22:40:24 +0900

    fix send email of invitation

--- app/workers/send_mail_worker.rb
+++
@@ -1,8 +0,0 @@
-class SendMailWorker
-  include Sidekiq::Worker
-
-  def perform(invitation_id)
-    invitation = Invitation.find(invitation_id)
-    AppMailer.send_invitation_email(invitation).deliver
-  end
-end
