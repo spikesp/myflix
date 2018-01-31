e31905ddb0a668d86a9296f0f7677475a240a91f
spike <weisuojiaoshou@gmail.com>
2018-01-31 21:51:25 +0900

    finish the show of add new page for admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125061024) do
+ActiveRecord::Schema.define(version: 20180131080417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180125061024) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
+    t.boolean  "admin"
   end
 
   create_table "videos", force: true do |t|
